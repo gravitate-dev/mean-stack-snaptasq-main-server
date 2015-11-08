937b1b883cf8def3ea559c7b5ab6ea21a1663f97
Robert <robertirribarren@gmail.com>
2015-11-08 15:10:54 -0800

    Revert "I converted the launchrock into the snaptasq"
    This reverts commit bb26b167ef0fc5beaecb72d9a0b68af445f9d9eb.

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -4,7 +4,7 @@ angular.module('snaptasqApp')
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'app/main/mainlandingemail.html',
+                templateUrl: 'app/main/main.html',
                 controller: 'MainCtrl',
             });
     });
--- client/app/requestBeta/requestbeta.controller.js
+++ client/app/requestBeta/requestbeta.controller.js
@@ -7,7 +7,7 @@ angular.module('snaptasqApp').controller('RequestBetaCtrl', function($scope, Bet
         restrict: 'ACE',
         // Replace the div with our template
         replace: false,
-        template: '<div class="sendgrid-subscription-widget" data-message-success="Thank you. We will contact you in the near future." data-submit-text="Submit" data-token="SAcCNqsWMVXRHCm7rnWYW0JG0Fg2Y%2BWPSWMRqnPS9LiJCHKEjYE4K3YeJRqW%2BkXz"></div>',
+        template: '<div class="sendgrid-subscription-widget" data-message-success="Thank you. You will be emailed a beta code when we start our beta trials." data-submit-text="Request Beta Code" data-token="SAcCNqsWMVXRHCm7rnWYW0JG0Fg2Y%2BWPSWMRqnPS9LiJCHKEjYE4K3YeJRqW%2BkXz"></div>',
         controller: function($scope) {
             ! function(d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0],
